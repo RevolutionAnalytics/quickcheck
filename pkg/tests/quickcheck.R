39b0fcc5049b5f1d85231b2db16bdb93e4466bf9
Antonio Piccolboni <antonio@piccolboni.info>
2014-05-05 14:21:14 -0700

    bring tests and skeletal man up to speed

--- pkg/tests/quickcheck.R
+++ pkg/tests/quickcheck.R
@@ -13,49 +13,45 @@
 # limitations under the License.
 
 library(quickcheck)
+library(functional)
 ## generator test thyself
-##tdgg.logical 
+##rlogical 
 unit.test(function(p.true) {
-  sample = tdgg.logical(p.true,lambda=1000)()
+  sample = rlogical(p.true,lambda=1000)
   binom.test(
     sum(sample),
     length(sample), 
     p.true,"two.sided")$p.value > 0.001},
-          generators = list(tdgg.distribution(runif, min = .1, max = .9)))
-##tdgg.integer 
+          generators = list(Curry(runif, n = 1, min = .1, max = .9)))
+##rinteger 
 unit.test(is.integer,
-          generators = list(tdgg.integer()))
-##tdgg.double 
+          generators = list(rinteger))
+##rdouble 
 unit.test(is.double,
-          generators = list(tdgg.double()))
-##tdgg.complex NAY
-##tdgg.character: 
+          generators = list(rdouble))
+##rcomplex NAY
+##rcharacter: 
 unit.test(is.character,
-          generators = list(tdgg.character()))
+          generators = list(rcharacter))
 
-##tdgg.raw
+##rraw
 unit.test(is.raw,
-          generators = list(tdgg.raw()))
+          generators = list(rraw))
 
-#tdgconstant
-unit.test(function(x) tdgg.constant(x)() == x, generators = list(tdgg.distribution(runif)))
-#tdgselect
-unit.test(function(l) is.element(tdgg.select(l)(), l), generators = list(tdgg.numeric.list(10)))
-#tdgmixture
-unit.test(function(n) is.element(tdgg.mixture(tdgg.constant(n), tdgg.constant(2*n))(), list(n,2*n)), 
-          generators = list(tdgg.distribution(runif)))
-#tdgdistribution
-unit.test(function(d) {
-  tdgd = tdgg.distribution(d)
-  ks.test(d(10000), sapply(1:10000, function(i) tdgd()))$p > 0.001},
-          generators = list(tdgg.select(list(runif, rnorm))))
-# tdgg.list
-# tdgg.data.frame 
-# tdgg.numeric.list
-# tdgg.fixed.list
-# tdgg.prototype
-# tdgg.prototype.list
-# tdgg.constant
-# tdgg.select
-# tdgg.mixture 
-# tdgg.any 
+#rconstant
+unit.test(function(x) rconstant(x) == x, generators = list(Curry(runif, n = 100)))
+#rselect
+unit.test(function(l) is.element(make.rselect(l)(), l), generators = list(rnumeric.list))
+#rmixture
+unit.test(function(n) is.element(make.rmixture(Curry(rconstant, n), Curry(rconstant, 2*n))(), list(n,2*n)), 
+          generators = list(Curry(runif, n = 1)))
+# rlist
+# rdata.frame 
+# rnumeric.list
+# rfixed.list
+# rprototype
+# rprototype.list
+# rconstant
+# rselect
+# rmixture 
+# rany 
