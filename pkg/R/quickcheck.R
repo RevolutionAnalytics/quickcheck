2646563a5395f23f0e301e726dfbec6b0d96b899
Antonio Piccolboni <antonio@piccolboni.info>
2014-12-18 17:50:32 -0800

    major rewrite with consisten interface, removed unused generators.

--- pkg/R/quickcheck.R
+++ pkg/R/quickcheck.R
@@ -1,55 +1,71 @@
 # Copyright 2011 Revolution Analytics
-#    
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-##main function
-unit.test = 
+sample = 
+	function(x, size, ...) 
+		x[base::sample(length(x), size = size, ...)]
+
+as.predicate =
+	function(expect)
+		function(...) {
+			tryCatch({expect(...); TRUE} , error = function(e) FALSE)}
+
+# import.expectations =
+# 	function(){
+# 		library(testthat)
+# 		names = grep("^expect", ls("package:testthat"), value = TRUE)
+# 		funs = lapply(names, function(n) as.predicate(get(n, envir = as.environment("package:testthat"))))
+# 	  mapply(Curry(assign, envir = parent.frame()), paste0("q", names), funs)
+# 	NULL}
+
+unit.test =
 	function(
-		predicate, 
-		generators = list(), 
-		sample.size = 10, 
-		precondition = function(...) TRUE, 
+		predicate,
+		generators = list(),
+		sample.size = 10,
+		precondition = function(...) TRUE,
 		stop = TRUE) {
 		set.seed(0)
-		test.cases = 
+		test.cases =
 			list(
-				predicate = predicate, 
-				env = 
+				predicate = predicate,
+				env =
 					do.call(
-						c, 
+						c,
 						lapply(
-							all.vars(body(predicate)), 
-							function(name) 
+							all.vars(body(predicate)),
+							function(name)
 								tryCatch({
 									val = list(eval(as.name(name)))
 									names(val) = name
-									val}, 
+									val},
 									error = function(e) NULL))),
-				cases = 
+				cases =
 					lapply(
-						1:sample.size, 
+						1:sample.size,
 						function(i) {
 							args = lapply(generators, function(a) a())
-							if(do.call(precondition, args) && 
+							if(do.call(precondition, args) &&
 								 	!do.call(
 								 		function(...){
 								 			tryCatch(
-								 				predicate(...), 
-								 				error = 
+								 				predicate(...),
+								 				error =
 								 					function(e){
-								 						traceback() 
-								 						print(e) 
-								 						FALSE})}, 
+								 						traceback()
+								 						print(e)
+								 						FALSE})},
 								 		args)){
 								print(
 									paste(
@@ -62,128 +78,130 @@ unit.test =
 			if (stop) {
 				tf = tempfile(tmpdir=".", pattern = "quickcheck")
 				save(test.cases, file = tf)
-				stop(file.path(getwd(),tf))}
+				stop(file.path(getwd(), tf))}
 			else test.cases}}
 
-## for short
-catch.out = function(...) capture.output(invisible(...))
-## test data  generators generators, 
 
 ## basic types
+
+rsize =
+	function(n) {
+		if(is.numeric(n))
+			rpois(1, lambda = n)
+		else
+			n(1)}
+
 rlogical = 
-	function(p.true = .5, lambda = 8) 
-		rbinom(1 + rpois(1, lambda),1,p.true) == 1
+	function(element = 0.5,	size = 10) {
+		if(is.logical(element))
+			element = as.numeric(element)
+		if(is.numeric(element)) {
+			p = element
+			element = 
+				function(n) 
+					as.logical(rbinom(n, size = 1, prob = p))}
+		element(rsize(size))}
 
-rinteger = 
-	function(elem.lambda = 100, len.lambda = 8) 
-		as.integer(rpois(1 + rpois(1, len.lambda), elem.lambda)) #why poisson? Why not? Why 100?
+rinteger =
+	function(element = 100, size = 10) {
+			if(is.numeric(element))
+				element = Curry(rpois, lambda = element)
+			element(rsize(size))}
 
-rdouble = 
-	function(min = -1, max = 1, lambda = 8) 
-		runif(1 + rpois(1, lambda), min, max)
+rdouble =
+	function(element = 0, size = 10) {
+		if(is.numeric(element))
+			element = Curry(rnorm, mean = element)
+		element(rsize(size))}
 
 ##rcomplex NAY
 
 rcharacter = 
-	function(str.lambda = 8, len.lambda = 8)  
-		sapply(
-			runif(1 + rpois(1, len.lambda)), 
-			function(x) substr(digest(x), 1, rpois(1, str.lambda)))
+	function(element = 10, size = 10) {
+		if(is.character(element))
+			element = nchar(element)
+		if(is.numeric(element))
+			element = Curry(rpois, lambda = element)
+		unlist(
+			sapply(
+				runif(rsize(size)),
+				function(x) substr(digest(x), 1, element(1))))}
 
-rraw = 
-	function(lambda = 8) 
-		sample(as.raw(0:255), rpois(1, lambda),  replace = TRUE)
-
+rraw =
+	function(element = as.raw(0:255), size = 10) {
+		if(is.raw(element))
+			element = Curry(sample, x = element, replace = TRUE)
+		element(rsize(size))} 
 
 rlist = 
-	function(
-		rdg = make.rany(max.level = max.level - 1, len.lambda = lambda), 
-		lambda = 10, 
-		max.level = 4) {
-		if(max.level > 0) 
-			replicate(rpois(1, lambda), rdg(), simplify = FALSE) 
-		else list()}
+	function(element = NULL, size = 5, depth = 4) {		
+		if(is.null(element)) 
+			element = 
+			Curry(rany, 
+						generators = 
+							list(
+								rlogical, 
+								rinteger, 
+								rdouble, 
+								rcharacter, 
+								rraw, 
+								Curry(rlist, size = size, depth = depth - 1)))
+		if(depth > 0) 
+			replicate(rsize(size), element(), simplify = FALSE)
+		else NULL}
 
-rdata.frame = 
+rdata.frame =
 	function(
-		row.lambda = 20, 
-		col.lambda = 5) {
-		ncol = 1 + rpois(1, col.lambda)
-		nrow = 1 + rpois(1, row.lambda)
-		gens = 
+		nrow = 10, 
+		ncol = 5, 
+		col.generators =
 			list(
-				rlogical, 
-				rinteger, 
-				rdouble, 
-				rcharacter)
-		columns = 
+				rlogical,
+				rinteger,
+				rdouble,
+				rcharacter)) {
+		nrow = rsize(nrow)
+		ncol = rsize(ncol)		
+		columns =
 			lapply(
-				sample(gens,ncol, replace=TRUE), 
-				function(g) replicate(nrow, g()[1], simplify = TRUE))
+				sample(col.generators, ncol, replace = TRUE),
+				function(g) 
+					replicate(nrow, g()[1], simplify = TRUE))
 		names(columns) = paste("col", 1:ncol)
 		do.call(data.frame, columns)}
 
-rDate = 
-	function(from = as.Date("0000/01/01"), to = Sys.Date(), len.lambda = 8, replace = TRUE) {
-		dates = as.Date(from):as.Date(to) 
-		as.Date(
-			dates[
+rDate =
+	function(element = list(from = as.Date("0000/01/01"), to = Sys.Date()), size = 10) {
+		if(is.list(element)) {
+			dates = as.Date(element$from):as.Date(element$to)
+			as.Date(
 				sample(
-					length(dates), 
-					rpois(1, len.lambda), replace = replace)], 
-			origin = as.Date("1970-01-01"))}
+					dates,
+					rsize(size), replace = TRUE),
+				origin = as.Date("1970-01-01"))}
+		else
+			element(rsize(size))}
 
 ## special distributions
 
-rnumeric.list = 
-	function(lambda = 100) 
-		lapply(1:rpois(1,lambda), function(i) runif(1))
+constant =
+	function(const = NULL)
+		function(size = 10)
+			replicate(size, const)
 
-make.rfixed.list = 
-	function(...) 
-		function() 
-			lapply(list(...), function(rdg) rdg())
-
-make.rprototype = 
-	function(prototype, generator = make.rany()) 
-		function() 
-			rapply(prototype, function(x) generator(), how = "list")
-
-make.rprototype.list = 
-	function(prototype, lambda = 10, generator) {
-		rdg = make.rprototype(prototype, generator)
-		function() 
-			replicate(rpois(1, lambda), rdg(), simplify = FALSE)}
-
-rconstant = 
-	function(const = NULL) 
-		const
-
-make.rselect = 
-	function(l) 
-		function() 
-			sample(l,1)[[1]]
+select = 
+	function(..., unlist = FALSE)
+		function(size = 10) {
+			sel = sample(list(...), size, replace = TRUE)
+			if(unlist) unlist(sel) else sel}
 
 ##combiners
-make.rmixture = 
-	function(...) 
-		function() 
+mixture =
+	function(...)
+		function()
 			sample(list(...), 1)[[1]]()
 
-## combine everything
-make.rany = 
-	function(
-		p.true = .5, 
-		int.lambda = 100, 
-		min = -1, 
-		max = 1, 
-		len.lambda = 10,  
-		list.rdg = 
-			make.rany(len.lambda = len.lambda, max.level= max.level - 1), max.level = 4) 
-		make.rmixture(
-			Curry(rlogical, p.true, len.lambda), 
-			Curry(rinteger, int.lambda, len.lambda), 
-			Curry(rdouble, min, max, len.lambda), 
-			Curry(rcharacter, len.lambda), 
-			Curry(rraw, len.lambda),
-			Curry(rlist, list.rdg, len.lambda, max.level))
+# combine everything
+rany =
+	function(generators = list(rlogical, rinteger, rdouble, rcharacter, rraw, rlist))
+		do.call(mixture, generators)()
